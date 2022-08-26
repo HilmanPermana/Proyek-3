f28d36ae42b336c7c67068056b0202034bfc3c8b
Hilman <hilmanpermana220@gmail.com>
2022-08-26 19:43:05 +0700

    Hasil Kerkom di kostan Naufal

--- NodeJS_Learn/Week-1/GettingInputFromUsers.js
+++
@@ -1,9 +0,0 @@
-console.log('Starting App.....')
-
-const command = process.argv[2]
-
-if (command === 'add') {
-    console.log('Adding Note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
