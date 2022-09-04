11e960060aa507ac7ced8935e38539f87d592a08
Hilman Permana <hilmanpermana220@gmail.com>
2022-09-05 01:58:06 +0700

    Update at PC

--- VueJS_Learn/Intro To Vue3/main.js
+++ VueJS_Learn/Intro To Vue3/main.js
@@ -8,9 +8,18 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton','30% WOOL', '20% Polyester' ],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'}
+                { id: 2234, color: 'green', image:'./assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg'},
             ]
         }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
+
     }
 })
